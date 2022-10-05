4a82599bce1c79dd99543c63f2fff801aa3f8bd8
Evgeny Vovk <Evgeny.vovk@mths.ca>
2022-10-05 14:51:04 +0000

    done with unit2-05 cpp

---
+++ ICS3U-Unit2-05.cpp
@@ -0,0 +1,38 @@
+// Copyright(c) 2022 Evgeny Vovk All rights reserved.
+//
+// Created by : Evgeny Vovk
+// Created on : October 2022
+// ICS3U-Unit2-05.cpp File, learning input processing and output in C++.
+
+#include <iostream>
+
+// global variable
+int variableX = 25;
+
+
+void localVariable() {
+    // this shows what happens with local variables
+
+    int variableX = 10;
+    int variableY = 30;
+    int variableZ = variableX + variableY;
+    std::cout << "Local variableX, variableY, variableZ: "
+              << variableX << "+" << variableY << "=" << variableZ << std::endl;
+}
+
+void globalVariable() {
+    // this shows what happens with global variables
+
+    variableX = variableX + 1;
+    int variableY = 30;
+    int variableZ = variableX + variableY;
+    std::cout << "Global variableX, variableY, variableZ: "
+              << variableX << "+" << variableY << "=" << variableZ << std::endl;
+}
+
+int main() {
+    // this function calls local and global
+
+    localVariable();
+    globalVariable();
+}
